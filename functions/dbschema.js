09416fe80ec6e34ac8d15ecdbe35e4f2c2568a84
OttoCodeBerlin <ottoschmidberlin@gmail.com>
2020-01-07 13:58:48 +0100

    First final commit

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this ist the scream body',
+      createdAt: '2020-01-06T19:20:51.956Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
