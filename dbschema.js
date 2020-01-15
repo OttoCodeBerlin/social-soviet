4b9c6c34b27d79d338ebc2a340f0d83f0b7b20de
OttoCodeBerlin <ottoschmidberlin@gmail.com>
2020-01-15 19:46:41 +0100

    Added comment functionality and routes

--- dbschema.js
+++ dbschema.js
@@ -19,5 +19,35 @@ let db = {
       likeCount: 5,
       commentCount: 2
     }
+  ],
+  comments: [
+    {
+      userHandle: 'user',
+      screamId: 'sodsfokqwkepoewkpowe',
+      body: 'nice one mate!',
+      createdAt: '2020-01-06T19:20:51.956Z'
+    }
+  ]
+}
+const userDetails = {
+  credentials: {
+    userId: 'fsdfsdfsd',
+    email: 'user@email.com',
+    handle: 'user',
+    createdAt: '2019-03-15T10:59:52.798Z',
+    imageUrl: 'image/sfsdgfdfgdfU/dfgdf',
+    bio: 'Gehts mir gut',
+    website: 'https://user.com',
+    location: 'Lonodn, UK'
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'hh705sdfsdfdsfs'
+    },
+    {
+      userHandle: 'user',
+      screamId: 'qw232423jhkjjkjk'
+    }
   ]
 }
--- functions/index.js
+++ functions/index.js
@@ -4,18 +4,22 @@ const app = require('express')()
 
 const FBauth = require('./utilities/fbauth')
 
-const { getAllScreams, postOneScream } = require('./handlers/screams')
-const { signup, login, uploadImage, addUserDetails } = require('./handlers/users')
+const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams')
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
 
 //Scream routes
 app.get('/screams', getAllScreams)
 app.post('/scream', FBauth, postOneScream)
+app.get('/scream/:screamId', getScream)
+app.post('/scream/:screamId/comment', FBauth, commentOnScream)
+
+//Todo: delete scream, like scream, unlike scream, comment on scream
 
 //User routes
 app.post('/signup', signup)
 app.post('/login', login)
-
 app.post('/user/image', FBauth, uploadImage)
 app.post('/user', FBauth, addUserDetails)
+app.get('/user', FBauth, getAuthenticatedUser)
 
 exports.api = functions.region('europe-west1').https.onRequest(app)
